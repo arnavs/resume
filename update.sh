9247eb079b25c4b4d82a7580edc93daa115966b7
Arnav Sood <5009477+arnavs@users.noreply.github.com>
2021-01-25 16:54:42 -0500

    Update Jan 25 21, 16:54:42

--- update.sh
+++ update.sh
@@ -2,6 +2,12 @@
 # Update website.
 commit_date=$(date '+%b %d %y, %H:%M:%S')
 
-git add .
+rm -r docs
+hugo
+echo "arnavsood.com" > docs/CNAME
+
+./directory-listing.sh
+
+git add -A
 git commit -m "Update ${commit_date}"
 git push
